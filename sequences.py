d2ed84f91b958694dccd35b411f86aaea85a3bc9
CalebBabione <caleb.b@tutanota.com>
2025-03-25 11:40:34 -0500

    created new file added some fun stuff to sequences.py

--- sequences.py
+++ sequences.py
@@ -2,7 +2,6 @@ name = "Harry"
 #Just like in C a string is just a fancy array.
 #You can use an array at the end of the variable to pull only 1 letter
 print(name[0])
-
 #how can we store multiple names?
 #well just use a 3d array silly!
 names = ["Harry", "Ron", "Hermione"]
@@ -10,3 +9,7 @@ names = ["Harry", "Ron", "Hermione"]
 print(names[0])
 #The below is how we will pull the n in "ron"
 print(names[1][2])
+#this is coupling this will pair data that is one unit
+coordinate = ([1.0, 2.0], [20.0, 30.0])
+#couples can also store arrays.
+print(coordinate[0][1])
