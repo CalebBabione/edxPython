59abda1be4bc308ad70685ddee80515bcc61e31a
CalebBabione <caleb.b@tutanota.com>
2025-03-25 11:27:07 -0500

    updated comments

--- conditions.py
+++ conditions.py
@@ -4,8 +4,10 @@
 #the below text will turn the "input" into an INT
 n = int(input("Number: "))
 
+#simple boolean if statements
 if n > 0:
     print("n is positive")
+#elif is short for "else if"
 elif n < 0:
     print("n is negative")
 else:
